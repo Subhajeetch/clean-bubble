2952b5d86f8b22599e72095ccd02324f35e6727a
Subhajeetch <subhajeetchowdhury954@gmail.com>
2025-04-29 20:23:03 +0530

    updated the main page

---
+++ frontend/app/page.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="relative min-h-screen bg-black flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">Clean Bubble</h1>
+      <span>- On development</span>
+    </div>
+  );
+}
