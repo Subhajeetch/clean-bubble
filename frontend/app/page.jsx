4ca4dd53b52c67cb33687a54475adf438415f0d0
Subhajeetch <subhajeetchowdhury954@gmail.com>
2025-04-30 15:58:15 +0530

    fixed spelling mistake

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="relative min-h-screen bg-black flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">Clean Bubble</h1>
-      <span>- On development</span>
+      <span>- In development</span>
     </div>
   );
 }
