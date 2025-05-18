c148a451eec0b0e42c86d32340ddae0f988104d1
Subhajeetch <subhajeetchowdhury954@gmail.com>
2025-05-18 21:39:29 +0530

    added some content

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,8 +1,47 @@
+
+
 export default function App() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">Clean Bubble</h1>
-      <span>- In development</span>
+    <main className="min-h-screen flex flex-col max-w-[1200px] mx-auto px-4">
+      <div className="flex items-center justify-center relative h-40">
+        <div className="w-60 h-60 md:w-80 md:h-80 rounded-full border-2 border-muted  flex items-center justify-center absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <img
+            src="/clean-bubble-logo.png"
+            alt="Clean Bubble Logo"
+            className="w-12 h-12 mt-18"
+          />
+        </div>
+
+      </div>
+
+      {/* main div */}
+      <div className="flex flex-col lg:flex-row">
+
+        <div className="order-2 lg:order-1 flex max-w-[300px] w-full flex flex-col">
+          <h1 className="text-6xl font-bold mt-8 flex flex-col gap-2 mb-14">
+            <span className="font-bold">Foaming</span>
+            <span>Hand</span>
+            <span>Sanitizer</span>
+          </h1>
+
+          <p className="price text-5xl mb-20">
+            <span itemProp="priceCurrency" content="PKR">₹</span>
+            <span itemProp="price" content="149.99">249.99</span>
+          </p>
+
+          <p className="text-dimmer-foreground">
+            Goodbye to sticky hands and hello to clean, soft skin!
+          </p>
+
+        </div>
+        <div className="order-1 lg:order-2 max-w-[500px] flex justify-center mx-auto bg-blue-500">
+          <img src="/clean-bubble-png-front.png" className="w-full mr-3"></img>
+        </div>
+
+
+        <div className="order-3 max-w-[300px] w-full  bg-amber-700">heyyyyyyyyyyyyy</div>
+      </div>
+      {/* main div end */}
     </main>
   );
 }
--- frontend/universal/mainHeader.jsx
+++ frontend/universal/mainHeader.jsx
@@ -15,64 +15,56 @@ import Image from "next/image";
 
 export default function MainHeader() {
     return (
-        <header className="absolute w-full top-0 left-0 h-16 bg-blue-400 flex items-center justify-between px-4">
-            <div className="flex items-center gap-3">
+        <header className="absolute z-50 w-full top-0 left-0 h-16 px-4">
+            <div className="w-full h-16 flex items-center justify-between  max-w-[1200px] mx-auto">
+                <div className="flex items-center gap-6">
 
-                <DropdownMenu>
-                    <DropdownMenuTrigger className="md:hidden"><Menu size={26} /></DropdownMenuTrigger>
-                    <DropdownMenuContent className="ml-2">
-                        <DropdownMenuLabel>Nevigation Menu</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                            <Link href="#" className="font-semibold">
-                                Our Story
-                            </Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <Link href="#" className="font-semibold">
-                                Contact
-                            </Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <Link href="#" className="font-semibold">
-                                Terms
-                            </Link>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="md:hidden"><Menu size={26} /></DropdownMenuTrigger>
+                        <DropdownMenuContent className="ml-2">
+                            <DropdownMenuLabel>Nevigation Menu</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <Link href="#" className="font-semibold">
+                                    Our Story
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href="#" className="font-semibold">
+                                    Contact
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href="#" className="font-semibold">
+                                    Terms
+                                </Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
 
-                <Link href="#" className="font-semibold hidden md:block">
-                    Our Story
-                </Link>
-                <Link href="#" className="font-semibold hidden md:block">
-                    Contact
-                </Link>
-                <Link href="#" className="font-semibold hidden md:block">
-                    Terms
-                </Link>
-            </div>
+                    <Link href="#" className="font-semibold hidden md:block">
+                        Our Story
+                    </Link>
+                    <Link href="#" className="font-semibold hidden md:block">
+                        Contact
+                    </Link>
+                    <Link href="#" className="font-semibold hidden md:block">
+                        Terms
+                    </Link>
+                </div>
 
-            <div>
+                <div className="flex items-center gap-3">
+                    <Link className="h-7 w-7 relative" href="#">
+                        <span className="h-4 w-4 rounded-full bg-orange-400 absolute top-[-7px] right-[-3px] flex items-center justify-center text-[12px] font-bold">3</span>
+                        <ShoppingCart size={26} />
+                    </Link>
 
-                <Image
-                    width={80}
-                    height={80}
-                    src="/clean-bubble-logo.png"
-                    alt="Clean Bubble Logo"
-                />
-
-            </div>
-            <div className="flex items-center gap-3">
-                <Link className="h-7 w-7 relative" href="#">
-                    <span className="h-4 w-4 rounded-full bg-orange-400 absolute top-[-7px] right-[-3px] flex items-center justify-center text-[12px] font-bold">3</span>
-                    <ShoppingCart size={26} />
-                </Link>
-
-                <Link href="#">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
+                    <Link href="#">
+                        <Button>
+                            Login
+                        </Button>
+                    </Link>
+                </div>
             </div>
         </header>
     );
